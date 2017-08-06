0e65f0c37af930df6e5def04b7dbaea6a1810c57
allencage <alingabriel.ciuca@gmail.com>
2017-08-06 17:50:23 +0300

    Initial commit

---
+++ stackoverflow-samples/ChildTaskInParentTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChildTaskInParentTask
+{
+    class Program
+    {
+
+        //Note - basically when you call parent.Wait() it in turn waits for any child in it to finish executing and if an exception gets thrown it will wrap that into AggregateException type
+        static void Main(string[] args)
+        {
+            Task parentTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Starting the child task");
+                Task child = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Child running, going for sleep for a second");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Will throw an error!");
+                    throw new Exception();
+                }, TaskCreationOptions.AttachedToParent);
+            });
+
+            try
+            {
+                Console.WriteLine("Waiting for parent task");
+                parentTask.Wait();
+                Console.WriteLine("Parent task finished executing");
+            }
+            catch (AggregateException ex)
+            {
+                //Note - the exception that gets thrown is of type AggregateException
+                Console.WriteLine("Exception is {0}", ex.InnerException.GetType());
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ stackoverflow-samples/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IFoo iFoo = GetIFoo();
+			if(iFoo is Foo)
+			{
+				Console.WriteLine($"foo is of type Foo + {iFoo.GetType()}");
+			}
+			Console.Read();
+		}
+
+		static IFoo GetIFoo()
+		{
+			var bar = new Bar
+			{
+				Name = "MyName",
+				Surname = "MySurname"
+			};
+			return bar;
+		}
+	}
+
+	public class Foo : IFoo
+	{
+		public string Name { get; set; }
+	}
+
+	public class Bar : Foo
+	{
+		public string Surname { get; set; }
+	}
+
+	public interface IFoo
+	{
+
+	}
+}
